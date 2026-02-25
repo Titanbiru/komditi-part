eb1f8a42ba52e7d713917b4672439f31763c37b2
Titanbiru <titaniumehe1@gmail.com>
2026-02-25 12:40:43 +0700

    save for later 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
+        User::updateOrCreate([
             'email' => 'test@example.com',
+        ], [
+            'name' => 'Main Admin',
+            'password' => Hash::make('admin12345'),
+            'role' => 'admin',
         ]);
     }
 }
